770d9e2907c9c479c1a206e219b9351f921b7319
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-04-22 14:36:55 -0300

    feat: add landing page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,22 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+
+    implementation("com.makeramen:roundedimageview:2.3.0")
+
+    implementation("jp.co.cyberagent.android:gpuimage:2.1.0")
+
+    implementation("org.koin:koin-androidx-viewmodel:2.0.0")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
 }
